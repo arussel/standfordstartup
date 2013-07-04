bffec86792220c387f3907b622d55fd15bf79fec
Russel <alexandre@russel.fr>
2013-07-04 21:28:09 +0200

    added index.html

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-/*app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});*/
+var buf = fs.readFileSync("index.html")
+app.get('/', function(request, response) {
+  response.send(buf.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
